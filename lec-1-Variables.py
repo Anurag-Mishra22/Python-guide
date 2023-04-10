f98a5349c78a44f4f8172e29f4823693442f5a07
Anurag Mishra <anurag2025mishra@gmail.com>
2023-04-10 22:47:34 +0530

    Variables

---
+++ lec-1-Variables.py
@@ -0,0 +1,8 @@
+x = 5
+x = "hi"
+
+# Be as descriptive in naming your variables as possible.
+# In Python convention says that your variable should be lowercased and seperated with underscores that is called snake casing.
+# In python variable names cannot start with special symbols or numbers
+# don't name your variables things like int,str,list,etc..
+# There is a special value in python for none meaning no value,and that has a special keyword None.
